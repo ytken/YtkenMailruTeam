ca923efd3718a187a9a291a843749c969137e373
ytken <1909dande.lion@gmail.com>
2020-11-17 11:56:52 +0300

    Main screen & navigation

--- app/src/main/java/ru/hse/dormitoryproject/MainActivity.kt
+++ app/src/main/java/ru/hse/dormitoryproject/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.hse.dormitoryproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.feedFragment, R.id.tasksFragment, R.id.favouritesFragment, R.id.profileFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
