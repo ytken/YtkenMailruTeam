4e51f7ef6bcd90bc0df9e0996558bcd1f8004470
Александр Галанов <60795996+alexstanchenko@users.noreply.github.com>
2020-11-30 13:54:34 +0300

    Add DataBase

--- app/src/main/java/ru/hse/dormitoryproject/MainActivity.kt
+++ app/src/main/java/ru/hse/dormitoryproject/MainActivity.kt
@@ -1,22 +1,45 @@
 package ru.hse.dormitoryproject
 
+import android.graphics.PostProcessor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import ru.hse.dormitoryproject.Utils.DataBase
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         val navController = findNavController(R.id.fragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.feedFragment, R.id.tasksFragment, R.id.favouritesFragment, R.id.profileFragment))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.feedFragment,
+                R.id.tasksFragment,
+                R.id.favouritesFragment,
+                R.id.profileFragment
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         bottomNavigationView.setupWithNavController(navController)
+
+
+
+        DataBase.writeToBase(PostObject("123","123","123","123",true), nameOfPageWork)
+       val objects: List<PostObject>? =  DataBase.readAllBase(nameOfPageWork)
+
+    }
+
+    companion object {
+        private const val  nameOfPageWork ="PageWork"
     }
 }
--- app/src/main/java/ru/hse/dormitoryproject/PostObject.kt
+++ app/src/main/java/ru/hse/dormitoryproject/PostObject.kt
@@ -1,11 +1,24 @@
 package ru.hse.dormitoryproject
 
-import java.io.FileDescriptor
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.IgnoreExtraProperties
 
+@IgnoreExtraProperties
 data class PostObject(
-    val title : String,
-    val descriptor: String,
-    val content : String,
-    val dateOfPublish: String,
-    val isFavorite : Boolean
-)
+    val title: String? = "",
+    val descriptor: String? = "",
+    val content: String? = "",
+    val dateOfPublish: String? = "",
+    val isFavorite: Boolean? = false
+) {
+    @Exclude
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "title" to title,
+            "descriptor" to descriptor,
+            "content" to content,
+            "dateOfPublish" to dateOfPublish,
+            "isFavorite" to isFavorite
+        )
+    }
+}
