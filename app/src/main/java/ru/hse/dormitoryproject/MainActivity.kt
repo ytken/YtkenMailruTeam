4fe2de8e39e52277e3e1f9bddfbe60c1a9812faa
ytken <1909dande.lion@gmail.com>
2020-12-23 00:49:29 +0500

    Interface of profiles & searching for friends

--- app/src/main/java/ru/hse/dormitoryproject/MainActivity.kt
+++ app/src/main/java/ru/hse/dormitoryproject/MainActivity.kt
@@ -1,13 +1,14 @@
 package ru.hse.dormitoryproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +21,22 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         toolbar.title = resources.getString(R.string.app_name)
         toolbar.isClickable = true
+
+        toolbar.setOnMenuItemClickListener {
+    when (it.itemId) {
+        R.id.go_to_search -> {
+            val intent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(intent)
+
+        }
+        R.id.action_cancel -> {
+            val prevFragment = supportFragmentManager.findFragmentByTag(resources.getString(R.string.tag_search_fragment))
+            if (prevFragment != null)
+                supportFragmentManager.beginTransaction().remove(prevFragment).commit()
+        }
+        else -> Toast.makeText(this@MainActivity, "No activity!", Toast.LENGTH_SHORT).show()
+        }
+    true
+    }
     }
 }
--- app/src/main/java/ru/hse/dormitoryproject/profileFragment.kt
+++ app/src/main/java/ru/hse/dormitoryproject/profileFragment.kt
@@ -1,27 +1,35 @@
 package ru.hse.dormitoryproject
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import ru.hse.dormitoryproject.friend_data.FriendItem
+import ru.hse.dormitoryproject.friend_data.FriendRepository
 
 class profileFragment : Fragment() {
 
+    var name : String = "Log"
+    var surname : String = "Login"
     var currencyNumber : Int = 0
     var rateNumber : Int = 0
     var linkVK : String = ""
 
+    lateinit var textName: TextView
+    lateinit var textSurname: TextView
     lateinit var textCurr: TextView
     lateinit var textRate: TextView
     lateinit var textVK: TextView
     lateinit var imageProf: ImageView
 
+    lateinit var buttonFriends: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        currencyNumber = 10
-        rateNumber = 3
-        linkVK = "http"
+
     }
 
     override fun onCreateView(
@@ -31,17 +39,40 @@ class profileFragment : Fragment() {
         val v: View = inflater.inflate(R.layout.fragment_profile, container, false)
         setHasOptionsMenu(true)
 
+        textName = v.findViewById(R.id.textViewName)
+        textSurname = v.findViewById(R.id.textViewSurname)
         textCurr = v.findViewById(R.id.textViewCurrency)
         textRate = v.findViewById(R.id.textViewRate)
         textVK = v.findViewById(R.id.textViewVKLink)
         imageProf = v.findViewById(R.id.imageViewProfile)
 
+        textName.text = name
+        textSurname.text = surname
         textCurr.text = currencyNumber.toString()
         textVK.text = linkVK
         textRate.text = rateNumber.toString()
         imageProf.setImageResource(R.drawable.nusha)
 
+        /*buttonFriends = v.findViewById(R.id.button_login_friends)
+        buttonFriends.setOnClickListener{
+            val searchFragment = SearchProfilesFragment()
+            Log.d("my", "Button clicked")
+            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment, searchFragment).addToBackStack(null).commit()
+        }*/
+
         return v
+    }
+
+    companion object {
+        fun newInstance(friendItem: FriendItem) = profileFragment().apply {
+            arguments = Bundle().apply {
+                name = friendItem.name
+                surname = friendItem.surname
+                currencyNumber = friendItem.currency
+                rateNumber = friendItem.rate
+                linkVK = friendItem.VK
+            }
+        }
     }
 
 }
